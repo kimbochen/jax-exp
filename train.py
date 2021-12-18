b9e2c8b14b7c1dcd135678dfd56d8318cb6f2b3b
Kimbo Chen <chentenghung@gmail.com>
2021-12-18 11:53:26 +0000

    Refactored optimizer to resemble optax.

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import pickle
 from dataclasses import dataclass
-from functools import partial
 
 import jax
 import jax.numpy as jnp
@@ -17,37 +16,43 @@ class TrainerConfig:
     batch_size: int
     lr: float
     b1: float = 0.9
-    b2: float = 0.99
+    b2: float = 0.999
     eps: float = 1e-8
 
 
-def init_model(model, seed):
-    params, treedef = jax.tree_flatten(model)
+def init_layer(layer, seed):
+    params, treedef = jax.tree_flatten(layer)
     keys = rand.split(seed, len(params))
     return treedef.unflatten(p(k) for p, k in zip(params, keys))
 
 def init_adam(tconf):
-    def adam(param, grad, mu, var, i):
-        mu = (1.0 - tconf.b1) * grad + tconf.b1 * mu
-        var = (1.0 - tconf.b2) * jnp.square(grad) + tconf.b2 * var
+    def update_model(model, opt_state):
+        mu, var, t = opt_state
+        def adam_update(param, mu, var):
+            m_hat = mu / (1.0 - tconf.b1 ** t)
+            v_hat = var / (1.0 - tconf.b2 ** t)
+            param = param - tconf.lr * m_hat / (jnp.sqrt(v_hat) + tconf.eps)
+            return param
+        return jax.tree_map(adam_update, model, mu, var)
 
-        m_hat = mu / (1.0 - tconf.b1 ** i)
-        v_hat = var / (1.0 - tconf.b2 ** i)
-        param = param - tconf.lr * m_hat / (jnp.sqrt(v_hat) + tconf.eps)
+    def update_opt_state(grads, opt_state):
+        mu, var, t = opt_state
 
-        return param, mu, var
+        update_mu = lambda mu, grad: (1.0 - tconf.b1) * grad + tconf.b1 * mu
+        mu = jax.tree_map(update_mu, mu, grads)
+
+        update_var = lambda var, grad: (1.0 - tconf.b2) * jnp.square(grad) + tconf.b2 * var
+        var = jax.tree_map(update_var, var, grads)
+
+        return mu, var, t + 1
 
     def init_opt_state(model):
         mu = jax.tree_map(lambda p: jnp.zeros_like(p), model)
         var = jax.tree_map(lambda p: jnp.zeros_like(p), model)
-        return mu, var, 1
+        return mu, var, 0
 
-    def split_tree(opt_tree, idx):
-        is_tuple = lambda x: isinstance(x, tuple)
-        tree = jax.tree_map(lambda t: t[idx], opt_tree, is_leaf=is_tuple)
-        return tree
+    return update_model, update_opt_state, init_opt_state
 
-    return adam, init_opt_state, split_tree
 
 def cross_entropy(model, x, y):
     logit = model(x)
@@ -58,17 +63,15 @@ def cross_entropy(model, x, y):
 
 
 def train(model, train_dl, tconf):
-    adam_i, init_opt_state, split_tree = init_adam(tconf)
+    update_model, update_opt_state, init_opt_state = init_adam(tconf)
     opt_state = init_opt_state(model)
 
     @jax.jit
     def step(model, xb, yb, opt_state):
         loss, grads = jax.value_and_grad(cross_entropy)(model, xb, yb)
-        mu, var, idx = opt_state
-        adam = partial(adam_i, i=idx)
-        opt_tree = jax.tree_map(adam, model, grads, mu, var)
-        model, mu, var = (split_tree(opt_tree, i) for i in range(3))
-        return loss, model, (mu, var, idx + 1)
+        opt_state = update_opt_state(grads, opt_state)
+        model = update_model(model, opt_state)
+        return loss, model, opt_state
 
     pbar = tqdm.trange(tconf.max_epoch)
     for epoch in pbar:
@@ -81,21 +84,22 @@ def train(model, train_dl, tconf):
 
     return model
 
+
 def main():
-    tconf = TrainerConfig(max_epoch=350, batch_size=256, lr=3e-3)
+    tconf = TrainerConfig(max_epoch=500, batch_size=128, lr=3e-3)
     text, codebook = process_dataset('data/input.txt', print_stats=False)
     mconf = GPTConfig(
-        d_embd=256, n_head=8, n_layer=8, block_size=128,
+        d_embd=128, n_head=4, n_layer=4, block_size=64,
         n_vocab=codebook.size
     )
     train_ds, _ = train_test_split(codebook, text, mconf.block_size)
 
+    model = init_layer(GPT(mconf), rand.PRNGKey(39))
     train_dl = Dataloader(train_ds, tconf.batch_size)
-    model = init_model(GPT(mconf), rand.PRNGKey(39))
     model = train(model, train_dl, tconf)
 
-    with open('ckpt_model.pkl', 'wb') as pkl_file:
-        pickle.dump(model, pkl_file)
+    with open('ckpt_model.pkl', 'wb') as file:
+        pickle.dump(model, file)
 
 if __name__ == '__main__':
     main()
